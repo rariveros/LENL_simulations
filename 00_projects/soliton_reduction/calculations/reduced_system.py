53174dca7d49be9a1ac4b51107ddefb239affb2f
Rafa <rafael.riveros.a@gmail.com>
2025-01-23 21:38:29 -0300

    Soliton Control

--- 00_projects/soliton_reduction/calculations/reduced_system.py
+++ 00_projects/soliton_reduction/calculations/reduced_system.py
@@ -17,7 +17,7 @@ def phis(alpha, beta, nu, mu, gamma, sigma, X, Y, x_grid, dx):
 
 
 if __name__ == '__main__':
-    dx = 0.2
+    dx = 0.1
     x_grid = np.arange(-40, 40, dx)
     Xs = np.arange(-0.1, 0.1, 0.02)
     Ys = np.arange(-0.2, 0.2, 0.02)
@@ -28,11 +28,11 @@ if __name__ == '__main__':
     I = np.argmin(np.abs(Xs))
     J = np.argmin(np.abs(Ys))
     print(Xs[I], Ys[J])
-    nus = np.arange(-0.2, 0.0, 0.005)
+    nus = np.arange(-0.1, 0.0, 0.005)
     EIGS = []
     for nu in nus:
-        print("nu = {}".format(nu))
-        [alpha, beta, nu, mu, gamma, sigma] = [6.524, 1, nu, 0.075, 0.18, 15]
+        print("nu =" + f"{nu:.{3}f}")
+        [alpha, beta, mu, nu, sigma, gamma] = [6.524, 1, 0.075, nu, 15, 0.18]
         gamma_x = gamma * np.exp(- x_grid ** 2 / (2 * sigma ** 2))
         F_01 = []
         F_02 = []
@@ -95,6 +95,45 @@ if __name__ == '__main__':
         D2_00 = G2[I, J]
 
         D1_10 = G1_10[I, J]
+        D1_01 = G1_01[I, J]
+        D2_10 = G2_10[I, J]
+        D2_01 = G2_01[I, J]
+
+        D1_20 = G1_20[I, J]
+        D1_11 = G1_11[I, J]
+        D1_02 = G1_02[I, J]
+        D2_20 = G2_20[I, J]
+        D2_11 = G2_11[I, J]
+        D2_02 = G2_02[I, J]
+
+        D1_30 = G1_30[I, J]
+        D1_21 = G1_21[I, J]
+        D1_12 = G1_12[I, J]
+        D1_03 = G1_03[I, J]
+        D2_30 = G2_30[I, J]
+        D2_21 = G2_21[I, J]
+        D2_12 = G2_12[I, J]
+        D2_03 = G2_03[I, J]
+
+        D1 = [D1_00, D1_10, D1_01, D1_20, D1_11, D1_02, D1_30, D1_21, D1_12, D1_03]
+        D2 = [D2_00, D2_10, D2_01, D2_20, D2_11, D2_02, D2_30, D2_21, D2_12, D2_03]
+        parameters = np.array([alpha, beta, mu, nu, sigma, gamma])
+
+        alpha_str = f"{alpha:.{3}f}"
+        beta_str = f"{beta:.{3}f}"
+        mu_str = f"{mu:.{3}f}"
+        nu_str = f"{nu:.{3}f}"
+        sigma_str = f"{sigma:.{2}f}"
+        gamma_str = f"{gamma:.{3}f}"
+        savefile = 'D:/mnustes_science/simulation_data/FD/soliton_reduced/alpha=' + alpha_str + '/beta=' + beta_str + '/mu=' + mu_str + '/nu=' + nu_str + '/gamma=' + gamma_str + '/sigma=' + sigma_str
+        if not os.path.exists(savefile):
+            os.makedirs(savefile)
+
+        np.savetxt(savefile + '/D1.txt', D1, delimiter=',')
+        np.savetxt(savefile + '/D2.txt', D2, delimiter=',')
+        np.savetxt(savefile + '/parameters.txt', parameters, delimiter=',')
+
+
 
 
 
