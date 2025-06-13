4396f06166ca0b5f743c5124954e7edc7b89d56c
Rafa <rafael.riveros.a@gmail.com>
2025-06-13 19:25:56 -0400

    + Several [06-13]

--- 00_projects/LLG/DMI_patterns/simulations/LLG_DMI.py
+++ 00_projects/LLG/DMI_patterns/simulations/LLG_DMI.py
@@ -10,47 +10,55 @@ if __name__ == '__main__':
     route = 'mnustes_science/simulation_data/FD'
     eq = 'LLG_DMI'
 
-    t_rate = 40
-    A = 1.0
-    D = 4.0 #0.625
-    gamma = 1.0
-    alpha = 0.015
+    t_rate = 2
+    A = 4.0
+    D = 0.0
+    alpha = 0.01
 
     hx = 0.0
     hy = 0.0
     hz = 1.0
 
     Kx = 0.0
-    Ky = 1.0
+    Ky = 20.0
     Kz = 0.0
 
-    dh = 0.3
+    dist = 50 #30.0
+    sigma = 15 #6
+    phi = np.pi
 
     w0 = np.sqrt(hz * (hz + Ky))
-    print(w0)
-    nu = 0.02#-0.002441
+    nu = 0.02
     omega = 2 * (w0 + nu) #9.1626
 
     h = [hx, hy, hz]
     K = [Kx, Ky, Kz]
-    L_x = 140
+    L_x = 360
     # Definición de la grilla
-    [tmin, tmax, dt] = [0, 1000, 0.01]
-    [xmin, xmax, dx] = [- L_x/2, L_x/2, 0.5]
+    [tmin, tmax, dt] = [0, 2000, 0.02]
+    [xmin, xmax, dx] = [- L_x/2, L_x/2, 1.0]
     t_grid = np.arange(tmin, tmax + dt, dt)
     x_grid = np.arange(xmin, xmax, dx)
     T = tmax
     Nt = t_grid.shape[0]
     Nx = x_grid.shape[0]
+    dh_0 = 0.37
+    dh = dh_0 * np.real((np.exp(- (x_grid - dist / 2) ** 2 / (2 * sigma ** 2)) + np.exp(1j * phi) * np.exp( - (x_grid + dist / 2) ** 2 / (2 * sigma ** 2))))
+
+    mu = alpha * (2 * hz + Ky)
+    gamma = dh_0 * (2 * hz + Ky) / omega
+    print("nu = " + str(nu))
+    print("mu = " + str(mu / 2))
+    print("gamma = " + str(gamma / 4))
 
     # Initial Conditions
-    m1_init = m2_init = 0.01 * (np.random.rand(Nx) - 0.5)
+    m1_init = m2_init = 0.05 * (np.random.rand(Nx) - 0.5)
     m3_init = np.sqrt(1 - m1_init ** 2 - m2_init ** 2)
 
     # Empaquetamiento de parametros, campos y derivadas para integración
     L = xmax - xmin
-    D1 = sparse_D_periodic(Nx, dx) #sparse_D_neumann(Nx, dx)
-    D2 = sparse_DD_periodic(Nx, dx) #sparse_DD_periodic(Nx, dx)
+    D1 = sparse_D_neumann(Nx, dx) #sparse_D_neumann(Nx, dx)
+    D2 = sparse_DD_neumann(Nx, dx) #sparse_DD_periodic(Nx, dx)
     operators = np.array([D1, D2])
     fields_init = [m1_init, m2_init, m3_init]
     grids = [t_grid, x_grid, 0]
@@ -92,6 +100,7 @@ if __name__ == '__main__':
     cbar = plt.colorbar(pcm, shrink=1)
     cbar.set_label('$m_x(x,t)$', rotation=0, size=25, labelpad=-27, y=1.12)
     plt.xlim([x_grid[0], x_grid[-1]])
+    plt.ylim([1900, t_grid[-1]])
     plt.xlabel('$x$', size='25')
     plt.ylabel('$t$', size='25')
     cbar.ax.tick_params(labelsize=15)
